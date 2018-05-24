bc2d9f54b7b122ce86e7f24357df40f5af102468
Ramon Navarro <rnavarro@soaint.com>
2018-05-23 22:20:07 -0400

    RN: Agregar Route, y containers Home y NotFound

---
+++ src/Routes.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+
+/*Containers*/
+import Home from "./containers/Home";
+import NotFound from "./containers/NotFound";
+
+export default () =>
+	<Switch>
+		<Route path="/" exact component={Home} />		
+		<Route component={NotFound} />
+	</Switch>;
---
+++ src/containers/Home.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+
+export default class Home extends Component {
+	render() {
+		return (
+			<div className="Home">
+				<div className="lander">
+					<h1>RegPul</h1>
+					<p><b>Reg</b>istro de pacientes con cancer de <b>Pul</b>m&#243;n</p>
+				</div>
+			</div>
+		);
+	}
+}
