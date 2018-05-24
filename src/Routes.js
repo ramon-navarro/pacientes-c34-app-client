1bf3f50f7ab7b3a11c1fbcb68033a9af55bb694d
Ramon Navarro <rnavarro@soaint.com>
2018-05-23 23:44:59 -0400

    RN: Agregar Login

--- src/Routes.js
+++ src/Routes.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 
 /*Containers*/
 import Home from "./containers/Home";
+import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 
 export default () =>
 	<Switch>
-		<Route path="/" exact component={Home} />		
+		<Route path="/" exact component={Home} />
+		<Route path="/login" exact component={Login} />
 		<Route component={NotFound} />
 	</Switch>;
---
+++ src/config.js
@@ -0,0 +1,17 @@
+export default {
+	/* No usado en la demo
+	s3: {
+		REGION: "YOUR_S3_UPLOADS_BUCKET_REGION",
+		BUCKET: "YOUR_S3_UPLOADS_BUCKET_NAME"
+	},*/
+	apiGateway: {
+		REGION: "us-east-1",
+		URL: "YOUR_API_GATEWAY_URL"
+	},
+	cognito: {
+		REGION: "us-east-1",
+		USER_POOL_ID: "us-east-1_FGOxZmmBu",
+		APP_CLIENT_ID: "k73n5117odejocpgonhb4jb8g",
+		IDENTITY_POOL_ID: "us-east-1:545bae22-4219-4909-8712-3a92acf0c2ed"
+	}
+};
