baebc2ed1e1ce0abf215c3faa10feebb97553f54
Ramon Navarro <rnavarro@soaint.com>
2018-06-12 01:22:38 -0400

    RN: Login, ChangePassword by Challenge, Agregar boton de cargando...

--- src/Routes.js
+++ src/Routes.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import AppliedRoute from "./components/AppliedRoute";
 
 /*Containers*/
 import Home from "./containers/Home";
 import Login from "./containers/Login";
 import NotFound from "./containers/NotFound";
 
+export default ({ childProps }) =>
+	<Switch>
+		<AppliedRoute path="/" exact component={Home} props={childProps}/>
+		<AppliedRoute path="/login" exact component={Login} props={childProps}/>
+		{ /* Finally, catch all unmatched routes */ }
+		<Route component={NotFound} />
+	</Switch>;
+	
+/* Antes del manejo de sesion
 export default () =>
 	<Switch>
 		<Route path="/" exact component={Home} />
 		<Route path="/login" exact component={Login} />
 		<Route component={NotFound} />
 	</Switch>;
+*/
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,5 +1,6 @@
-import React, { Component } from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import React, { Component, Fragment} from "react";
+import {FormGroup, FormControl, ControlLabel, HelpBlock } from "react-bootstrap";
+import LoaderButton from "../components/LoaderButton";
 import "./Login.css";
 import { Auth } from "aws-amplify";
 
@@ -8,12 +9,25 @@ export default class Login extends Component {
 		super(props);
 		this.state = {
 			email: "",
-			password: ""
+			password: "",
+			newPassword: "",
+			newPasswordRequired: false,
+			isLoading: false		
 		};
+		this.user="";
+		this.data="";
 	}
 	
 	validateForm() {
 		return this.state.email.length > 0 && this.state.password.length > 0;
+	}
+	
+	validatePassword(){
+		var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\.\+!@#\$%\^&\*])(?=.{8,})");
+		const length = this.state.newPassword.length;
+		if (strongRegex.test(this.state.newPassword)) return 'success';		
+		else if (length > 0) return 'error';
+		return null;
 	}
 	
 	handleChange = event => {
@@ -23,12 +37,35 @@ export default class Login extends Component {
 	}
 	
 	handleSubmit = async event => {
-		event.preventDefault();
-		try {
-			await Auth.signIn(this.state.email, this.state.password);
-			alert("Logged in");
-		} catch (e) {
-			alert(e.message);
+		event.preventDefault();		
+		this.setState({ isLoading: true });
+		if(this.state.newPasswordRequired){		
+			try {				
+				this.data = await Auth.completeNewPassword(this.user, this.state.newPassword, this.user.challengeParam.requiredAttributes);
+					console.log(this.data);			
+					this.setState({newPasswordRequired: false});
+					this.props.userHasAuthenticated(true);
+					this.props.history.push("/");							
+			} catch (e) {
+				alert(e.message);
+				this.setState({ isLoading: false });
+			}			
+		}else{
+			try {
+				this.user = await Auth.signIn(this.state.email, this.state.password);
+				console.log(this.user);			
+				if(this.user.challengeName='NEW_PASSWORD_REQUIRED' && this.user.signInUserSession==null){
+					alert("Debe cambiar Password");
+					this.setState({newPasswordRequired: true});
+					this.setState({ isLoading: false });
+				}else{
+					this.props.userHasAuthenticated(true);
+					this.props.history.push("/");
+				}
+			} catch (e) {
+				alert(e.message);
+				this.setState({ isLoading: false });
+			}
 		}
 	}
 	
@@ -43,6 +80,7 @@ export default class Login extends Component {
 							type="email"
 							value={this.state.email}
 							onChange={this.handleChange}
+							disabled={this.state.newPasswordRequired}
 						/>
 					</FormGroup>
 					<FormGroup controlId="password" bsSize="large">
@@ -51,11 +89,30 @@ export default class Login extends Component {
 							value={this.state.password}
 							onChange={this.handleChange}
 							type="password"
+							disabled={this.state.newPasswordRequired}
 						/>
 					</FormGroup>
-					<Button block bsSize="large" disabled={!this.validateForm()} type="submit">
-						Login
-					</Button>
+					{this.state.newPasswordRequired
+						?
+							<Fragment>
+							<FormGroup controlId="newPassword" bsSize="large" validationState={this.validatePassword()}>
+								<ControlLabel>New Password</ControlLabel>
+								<FormControl
+									autoFocus
+									value={this.state.newPassword}
+									onChange={this.handleChange}
+									type="password"
+								/>
+								<FormControl.Feedback />
+								<HelpBlock>Minimum length 8</HelpBlock>
+								<HelpBlock>Require numbers and special character</HelpBlock>								
+								<HelpBlock>Require uppercase and lowercase letters</HelpBlock>								
+							</FormGroup>
+							<LoaderButton block bsSize="large" bsStyle="primary" disabled={!(this.validatePassword()=='success')} type="submit" isLoading={this.state.isLoading} text="Change Password" loadingText="Cargando…"/>
+							</Fragment>
+						:
+							<LoaderButton block bsSize="large" bsStyle="primary" disabled={!this.validateForm()} type="submit" isLoading={this.state.isLoading} text="Login" loadingText="Cargando…"/>
+					}					
 				</form>
 			</div>
 		);
